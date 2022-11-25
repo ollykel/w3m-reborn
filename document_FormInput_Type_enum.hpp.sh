f6a8c3c9dc9643cc7cf516ad2e2535717c9d93a0
Oliver A. Kelton <oakelton@gmail.com>
2022-11-24 16:01:11 -0800

    created code generation script for enum class Document::FormType::Type

---
+++ document_FormInput_Type_enum.hpp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Generates a C++ header file containing an enum class Type, which contains
+# the tokens read from stdin. Formatted output printed to stdout.
+#
+# Makefile should feed this script appropriate input (i.e. from a .txt
+# file) and output to the appropriate destination (i.e. a .hpp file).
+
+read token
+
+cat << _EOF_
+// Does not use include guards, by design.
+// Should be #include'd at the appropriate location in document.hpp.
+
+enum class Type
+{
+    ${token} = 0
+_EOF_
+
+while read token; do
+cat << _EOF_
+    ,${token}
+_EOF_
+done
+
+cat << _EOF_
+}// end enum class Type
+_EOF_
