6eaffb2257d9d80c0223871c9548ee6244d39e78
Oliver A. Kelton <oakelton@gmail.com>
2022-11-26 20:49:38 -0800

    created document_html_entMap.hpp.sh code generation script for html entities

---
+++ document_html_entMap.hpp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat << _EOF_
+static const std::map<string, unsigned>   entMap  =
+{
+_EOF_
+
+awk -F '[ \t]*,[ \t]*' '{printf("\t{\"%s\", %s},\n", $1, $2)}'
+
+cat << _EOF_
+};// end static const std::map<string, unsigned>   entMap
+_EOF_
