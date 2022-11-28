3fcf48d012076102b307d79f53db1c99de2e3631
Oliver A. Kelton <oakelton@gmail.com>
2022-11-27 18:39:43 -0800

    dummy implementation of tests/proto_01.cpp

---
+++ tests/proto_01.cpp
@@ -0,0 +1,116 @@
+#include <cstdio>
+#include <climits>
+
+#include <curses.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "../deps.hpp"
+#include "../html_parser.hpp"
+#include "../dom_tree.hpp"
+#include "../document_html.hpp"
+
+struct Config
+{
+    Document::Config        document;
+};// end struct Config
+
+// === Function Prototypes ================================================
+int runtime(const Config& cfg);
+
+// === main ===============================================================
+//
+// ========================================================================
+int main(const int argc, const char **argv, const char **envp)
+{
+    using namespace std;
+
+    int         ret         = EXIT_FAILURE;
+    int         wstatus;
+    pid_t       child;
+    Config      config      = {
+        {
+            // inputWidth
+            {
+                40,         // def
+                0,          // min
+                SIZE_MAX,   // max
+            },
+        },
+    };
+
+    // initialize screen
+    initscr();
+    cbreak();
+    noecho();
+
+    intrflush(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+    wtimeout(stdscr, 100);
+    curs_set(0);
+    start_color();
+
+    switch ((child = fork()))
+    {
+        case 0:
+            // we are child process
+            return runtime(config);
+        case -1:
+            // could not fork
+            cerr << "ERROR: could not spawn child process" << endl;
+            ret = EXIT_FAILURE;
+        default:
+            // we are parent
+            waitpid(child, &wstatus, 0x00);
+
+            ret = WEXITSTATUS(wstatus);
+    }// end switch
+
+finally:
+    // reset screen
+    use_default_colors();
+    curs_set(1);
+    endwin();
+
+    return ret;
+}// end main
+
+// === Function Definitions ===============================================
+
+int runtime(const Config& cfg)
+{
+    using namespace std;
+
+    const string    msg01   = "Hello World!";
+    const string    msg02   = "Press 'q' to exit.";
+
+    char    key;
+
+    // TODO: meaningfully implement
+
+    werase(stdscr);
+
+    // print messages
+    for (int i = 0; i < msg01.size(); ++i)
+    {
+        mvwaddch(stdscr, 0, i, msg01[i]);
+    }// end for
+    for (int i = 0; i < msg02.size(); ++i)
+    {
+        mvwaddch(stdscr, 1, i, msg02[i]);
+    }// end for
+    wrefresh(stdscr);
+
+    // wait for keypress
+    while (true)
+    {
+        switch ((key = wgetch(stdscr)))
+        {
+            case 'q':
+            case 'Q':
+                return EXIT_SUCCESS;
+        }// end switch
+    }// end while
+
+    return EXIT_SUCCESS;
+}// end runtime
