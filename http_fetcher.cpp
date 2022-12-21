15e82f258abf215462642a99603506076355662c
Oliver A. Kelton <oakelton@gmail.com>
2022-12-20 23:10:22 -0800

    implemented HttpFetcher

---
+++ http_fetcher.cpp
@@ -0,0 +1,149 @@
+#include <cctype>
+#include <map>
+
+#include "deps.hpp"
+#include "command.hpp"
+#include "uri.hpp"
+
+#include "http_fetcher.hpp"
+
+#define     READ_LEN    0x1000
+
+// === class HttpFetcher Implementation ===================================
+//
+// ========================================================================
+
+// --- public constructors ------------------------------------------------
+
+HttpFetcher::HttpFetcher(
+    const string& shellCommand,
+    const string& urlEnv
+)
+{
+    m_cmd = Command(shellCommand).set_stdout_piped(true);
+    m_urlEnv = urlEnv;
+}// end type constructor
+
+// --- public accessors ---------------------------------------------------
+
+auto HttpFetcher::fetch_url(
+    header_type& headers,
+    const Uri& url
+) const -> std::vector<char>
+{
+    using namespace std;
+
+    std::vector<char>       body            = {};
+    Command                 cmd             = m_cmd;
+    string                  currLine        = {};
+    size_t                  idx             = 0;
+
+    // set up command
+    cmd.set_env(m_urlEnv, url.str());
+
+    auto        sproc       = cmd.spawn();
+
+    // first, read headers
+    while (getline(sproc.stdout(), currLine))
+    {
+        header_key_type             key         = {};
+        header_value_type           value       = {};
+        size_t                      idx         = 0;
+        string::const_iterator      iter;
+
+        // remove terminal carriage return
+        if ((not currLine.empty()) and (currLine.back() == '\r'))
+        {
+            currLine.pop_back();
+        }
+
+        // if line is empty, break
+        if (currLine.empty())
+        {
+            break;
+        }
+
+        if (currLine.find(':') == string::npos)
+        {
+            continue;
+        }
+
+        #define     BREAK_IF_NPOS(VAR) \
+                    if ((VAR) >= string::npos) { break; }
+        #define     WS      " \t"
+
+        // parse key
+        {
+            size_t  beg     = 0;
+            size_t  end;
+
+            beg = currLine.find_first_not_of(WS);
+            end = currLine.find(':', beg);
+            idx = end + 1;
+            end = currLine.find_last_not_of(WS, end - 1, end - beg);
+
+            if (string::npos == end)
+            {
+                end = beg;
+            }
+            else
+            {
+                ++end;
+            }
+
+            key = currLine.substr(beg, end - beg);
+        }
+
+        // parse values
+        while (idx < currLine.size())
+        {
+            size_t      beg     = idx;
+            size_t      end;
+
+            idx = currLine.find_first_not_of(WS, beg);
+            BREAK_IF_NPOS(beg);
+            beg = idx;
+
+            end = currLine.find(';');
+            BREAK_IF_NPOS(end);
+            idx = end + 1;
+
+            end = currLine.find_last_not_of(WS, end - 1, end - beg);
+
+            if (string::npos == end)
+            {
+                end = beg;
+            }
+            else
+            {
+                ++end;
+            }
+
+            value.push_back(currLine.substr(beg, end + 1));
+        }// end while
+
+        #undef      WS
+        #undef      BREAK_IF_NPOS
+        headers[key] = value;
+    }// end while
+
+    // next, read body
+    while (sproc.stdout())
+    {
+        char    *buf;
+
+        body.resize(body.size() + READ_LEN);
+        buf = &body.back() + 1 - READ_LEN;
+
+        sproc.stdout().read(buf, READ_LEN);
+    }// end while
+
+    if (not body.empty())
+    {
+        body.resize(body.size() + sproc.stdout().gcount() - READ_LEN);
+    }
+
+    sproc.stdout().close();
+    sproc.wait();
+    return body;
+}// end HttpFetcher::fetch_url
---
+++ http_fetcher.hpp
@@ -0,0 +1,37 @@
+#ifndef __HTTP_FETCHER_HPP__
+#define __HTTP_FETCHER_HPP__
+
+#include <map>
+
+#include "deps.hpp"
+#include "command.hpp"
+#include "uri.hpp"
+
+class HttpFetcher
+{
+    public:
+        // --- public member types ----------------------------------------
+        typedef     string                  header_key_type;
+        typedef     std::vector<string>     header_value_type;
+        typedef     std::map<header_key_type, header_value_type>
+                                            header_type;
+
+        // --- public constructors ----------------------------------------
+        HttpFetcher(
+            const string& shellCommand,
+            const string& urlEnv
+        );
+
+        // --- public accessors -------------------------------------------
+        auto fetch_url(
+            header_type& headers,
+            const Uri& url
+        ) const -> std::vector<char>;
+
+    private:
+        // --- private member variables -----------------------------------
+        Command     m_cmd;
+        string      m_urlEnv;
+};// end class HttpFetcher
+
+#endif
